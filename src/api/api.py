6239a60687b53c0737791e7aa02a9fbbf9666164
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2022-06-13 20:17:45 +0200

    chore: add api module

---
+++ src/api/api.py
@@ -0,0 +1,15 @@
+def read_episode():
+    return "dummy_data"
+
+
+def read_episodes():
+    """Read list of episode from database"""
+    return "dummy_data"
+
+
+def create_episode():
+    return "dummy_data"
+
+
+def delete_episode():
+    return "dummy_data"
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from typing import Union
-from helloworld import msg
+
+from api import api
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
@@ -13,13 +15,17 @@ def read_root():
 def read_episode(episode_id: int, q: Union[str, None] = None):
     return {"episode_id": episode_id, "q": q}
 
+
 @app.get("/episodes")
 def read_episodes():
-    return {"data": "return list of episodes"}
+    data = api.read_episodes()
+    return {"data": data}
+
 
 @app.post("/episodes")
 def create_episode():
     return {"data": "return a full episode here"}
+
 
 @app.delete("/episodes/{episode_id}")
 def delete_episode(episode_id: int):
