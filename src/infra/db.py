0e3594e6a563941f81a1a1bcc2489f1b47a2d420
yunjae <yunjae.oh.nl@gmail.com>
2022-06-19 16:07:19 +0200

    chore: remove comments and print

--- src/infra/db.py
+++ src/infra/db.py
@@ -2,5 +2,4 @@ from sqlalchemy import create_engine
 
 from config import DB_HOST, DB_USER, DB_PWD
 
-print(DB_USER)
 engine = create_engine(f"postgresql://{DB_USER}:{DB_PWD}@{DB_HOST}:5432/episodedb")
